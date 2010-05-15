213f0172a24426866fb7c5a923da448925b8bd05
brett <brett@failurefiles.com>
2010-05-14 18:56:06 -0700

    update

--- Build/Build.bat
+++ Build/Build.bat
@@ -4,4 +4,5 @@ msbuild ..\Floe.UI\Floe.UI.csproj /P:Configuration=Release
 copy ..\Floe.UI\bin\Release\*.exe .
 copy ..\Floe.UI\bin\Release\*.dll .
 candle setup.wxs -ext WiXNetFxExtension
-light setup.wixobj -ext WiXNetFxExtension
+light setup.wixobj -ext WiXNetFxExtension -cultures:en-US -ext WixUIExtension.dll -dWixUILicenseRtf=License.rtf
+
