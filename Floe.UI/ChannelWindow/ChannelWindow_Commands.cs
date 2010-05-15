e1b112b54c53c482fae40711667013255b02b143
brett <brett@failurefiles.com>
2010-05-14 18:39:34 -0700

    update

---
+++ Floe.UI/ChannelWindow/ChannelWindow_Commands.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+using Floe.Net;
+
+namespace Floe.UI
+{
+	public partial class ChannelWindow : Window
+	{
+		private void ExecuteChat(object sender, ExecutedRoutedEventArgs e)
+		{
+			this.BeginInvoke(() => App.Create(this.Control.Session, new IrcTarget((string)e.Parameter), true));
+		}
+	}
+}
---
+++ Floe.UI/ChatControl/ChatControl_WndProc.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace Floe.UI
+{
+	public partial class ChatControl : UserControl
+	{
+
+	}
+}
