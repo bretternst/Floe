49693cb4fa64fdc3f9d6fc6173eae4626f0e5a5a
brett <brett@failurefiles.com>
2011-03-07 23:02:34 -0800

    refactor nickname list to add more visual options

---
+++ Floe.UI/ChatControl/NicknameItem.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Floe.UI
+{
+	[Flags]
+	public enum ChannelLevel
+	{
+		Normal = 0,
+		Voice = 1,
+		HalfOp = 2,
+		Op = 4
+	}
+
+	public class NicknameItem : DependencyObject, IComparable<NicknameItem>, IComparable
+	{
+		private static char[] NickSpecialChars = new[] { '[', ']', '\\', '`', '_', '^', '{', '|', '}' };
+		public static bool IsNickChar(char c)
+		{
+			return char.IsLetterOrDigit(c) || NickSpecialChars.Contains(c);
+		}
+
+		public NicknameItem(ChannelLevel level, string nick)
+		{
+			this.Nickname = nick;
+			this.Level = level;
+		}
+
+		public NicknameItem(string nick)
+		{
+			var level = ChannelLevel.Normal;
+			int i = 0;
+			for (i = 0; i < nick.Length && !IsNickChar(nick[i]); i++)
+			{
+				switch (nick[0])
+				{
+					case '@':
+						level |= ChannelLevel.Op;
+						break;
+					case '%':
+						level |= ChannelLevel.HalfOp;
+						break;
+					case '+':
+						level |= ChannelLevel.Voice;
+						break;
+				}
+			}
+			if (i < nick.Length)
+			{
+				nick = nick.Substring(i);
+			}
+			else
+			{
+				nick = "";
+			}
+			this.Nickname = nick;
+			this.Level = level;
+		}
+
+		public static DependencyProperty IsVoiceChatProperty = DependencyProperty.Register("IsVoiceChat", typeof(bool), typeof(NicknameItem));
+		public bool IsVoiceChat
+		{
+			get { return (bool)this.GetValue(IsVoiceChatProperty); }
+			set { this.SetValue(IsVoiceChatProperty, value); }
+		}
+
+		public string Nickname { get; set; }
+		public ChannelLevel Level { get; set; }
+		public string NickWithLevel { get { return this.ToString(); } }
+
+		private ChannelLevel HighestLevel
+		{
+			get
+			{
+				if ((this.Level & ChannelLevel.Op) > 0)
+				{
+					return ChannelLevel.Op;
+				}
+				else if ((this.Level & ChannelLevel.HalfOp) > 0)
+				{
+					return ChannelLevel.HalfOp;
+				}
+				else if ((this.Level & ChannelLevel.Voice) > 0)
+				{
+					return ChannelLevel.Voice;
+				}
+				else
+				{
+					return ChannelLevel.Normal;
+				}
+			}
+		}
+
+		public override string ToString()
+		{
+			string prefix;
+			switch (this.HighestLevel)
+			{
+				case ChannelLevel.Op:
+					prefix = "@";
+					break;
+				case ChannelLevel.HalfOp:
+					prefix = "%";
+					break;
+				case ChannelLevel.Voice:
+					prefix = "+";
+					break;
+				default:
+					prefix = "";
+					break;
+			}
+
+			return prefix + this.Nickname;
+		}
+
+		public int CompareTo(NicknameItem other)
+		{
+			if (other == null)
+			{
+				return 1;
+			}
+			if (this.HighestLevel == other.HighestLevel)
+			{
+				return string.Compare(this.Nickname, other.Nickname, StringComparison.InvariantCultureIgnoreCase);
+			}
+			else
+			{
+				return (int)other.HighestLevel - (int)this.HighestLevel;
+			}
+		}
+
+		public int CompareTo(object obj)
+		{
+			return this.CompareTo(obj as NicknameItem);
+		}
+	}
+}
---
+++ Floe.UI/ChatControl/NicknameList.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Floe.Net;
+
+namespace Floe.UI
+{
+	public class NicknameList : KeyedCollection<string, NicknameItem>, INotifyCollectionChanged
+	{
+		public NicknameList()
+			: base(StringComparer.OrdinalIgnoreCase)
+		{
+		}
+
+		public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+		public void AddRange(IEnumerable<string> nicks)
+		{
+			var items = nicks.Select((n) => new NicknameItem(n)).ToList();
+			foreach (var item in items)
+			{
+				this.Add(item);
+			}
+		}
+
+		public void Add(string nick)
+		{
+			this.Add(new NicknameItem(nick));
+		}
+
+		public void ChangeNick(string oldNick, string newNick)
+		{
+			var item = this[oldNick];
+			if (item != null)
+			{
+				item.Nickname = newNick;
+				this.ChangeItemKey(item, newNick);
+				var idx = this.IndexOf(item);
+				this.RefreshItem(idx);
+			}
+		}
+
+		public void ProcessMode(IrcChannelMode mode)
+		{
+			var mask = ChannelLevel.Normal;
+			switch (mode.Mode)
+			{
+				case 'o':
+					mask = ChannelLevel.Op;
+					break;
+				case 'h':
+					mask = ChannelLevel.HalfOp;
+					break;
+				case 'v':
+					mask = ChannelLevel.Voice;
+					break;
+			}
+
+			if (mask != ChannelLevel.Normal)
+			{
+				var item = this[mode.Parameter];
+				if (item != null)
+				{
+					item.Level = mode.Set ? item.Level | mask : item.Level & ~mask;
+					var idx = this.IndexOf(item);
+					this.RefreshItem(idx);
+				}
+			}
+		}
+
+		protected override string GetKeyForItem(NicknameItem item)
+		{
+			return item.Nickname;
+		}
+
+		protected override void SetItem(int index, NicknameItem item)
+		{
+			var oldItem = this[index];
+			base.SetItem(index, item);
+			this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, item, oldItem));
+		}
+
+		protected override void InsertItem(int index, NicknameItem item)
+		{
+			base.InsertItem(index, item);
+			this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, item, index));
+		}
+
+		protected override void ClearItems()
+		{
+			base.ClearItems();
+			this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+		}
+
+		protected override void RemoveItem(int index)
+		{
+			var item = this[index];
+			base.RemoveItem(index);
+			this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, item));	
+		}
+
+		private void RefreshItem(int idx)
+		{
+			var item = this[idx];
+			this.SetItem(idx, new NicknameItem(""));
+			this.SetItem(idx, item);
+		}
+
+		private void OnCollectionChanged(NotifyCollectionChangedEventArgs args)
+		{
+			var handler = this.CollectionChanged;
+			if (handler != null)
+			{
+				handler(this, args);
+			}
+		}
+	}
+}
